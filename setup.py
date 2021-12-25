43429a3ec636ac2b4f1c67c9ca4cf92784eeb086
Ryang Sohn <loop.infinitely@gmail.com>
2021-12-25 18:36:15 +0900

    Add rudimentary setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import find_packages, setup
+
+import os
+
+
+def read(rel_path: str) -> str:
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, rel_path)) as fp:
+        return fp.read()
+
+
+def get_version(rel_path: str) -> str:
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="boj-checker",
+    version=get_version("src/boj_checker/__init__.py"),
+    description="Test BOJ solution using sample IOs.",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Utilities",
+    ],
+    url="https://github.com/sohnryang/boj-checker",
+    project_urls={"Source": "https://github.com/sohnryang/boj-checker"},
+    author="Ryang Sohn",
+    author_email="loop.infinitely@gmail.com",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    entry_points={"console_scripts": []},
+    python_requires=">=3.7",
+    install_requires=["requests", "colorama", "beautifulsoup4", "xdg"],
+    zip_safe=False,
+)
